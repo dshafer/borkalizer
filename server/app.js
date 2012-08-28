f830bd812596fb9d7e1c428a8108a8b0877aedc2
Drew Shafer <drew@bluegrass.drewshafer.com>
2012-08-27 22:11:38 -0500

    server fixes

--- server/app.js
+++ server/app.js
@@ -10,37 +10,43 @@ var classified_log = 'classified.log';
 var app_log = 'app.log';
 var port = 8080;
 
-console.log("reading initial classification data from classified.log\n");
 var bayes = new classifier.Bayesian();
-var input = fs.createReadStream(classified_log);
-var remaining = '';
-var trainingCount = 0;
-input.on('data', function(data){
-  remaining += data;
-  var index = remaining.indexOf('>>>>');
-  var last = 0;
-  while(index>-1){
-    var line = remaining.substring(last,index);
-    last = index+4;
-    if(line.length>0){
-      trainLine(line);
-    }
-    index = remaining.indexOf('>>>>', last);
-  }
-  remaining = remaining.substring(last);
-});
-
-input.on('end', function() {
-  if(remaining.length>0){
-    trainLine(remaining);
-  }
-  console.log('finished initial training; count is ' + trainingCount);
+if(!fs.existsSync(classified_log)){
+  console.log("no initial classification data available at classified.log\n");
   startServer();
-});
+} else {
+  console.log("reading initial classification data from classified.log\n");
+
+  var input = fs.createReadStream(classified_log);
+  var remaining = '';
+  var trainingCount = 0;
+  input.on('data', function(data){
+    remaining += data;
+    var index = remaining.indexOf('>>>>');
+    var last = 0;
+    while(index>-1){
+      var line = remaining.substring(last,index);
+      last = index+4;
+      if(line.length>0){
+        trainLine(line);
+      }
+      index = remaining.indexOf('>>>>', last);
+    }
+    remaining = remaining.substring(last);
+  });
+  
+  input.on('end', function() {
+    if(remaining.length>0){
+      trainLine(remaining);
+    }
+    console.log('finished initial training; count is ' + trainingCount);
+    startServer();
+  });
+}
 
 function trainLine(line){
   var trainingData = JSON.parse(line);
-  bayes.train(trainingData.fingerprint, trainingData.classification);
+  bayes.train(trainingData.fingerprint.data, trainingData.classification);
   trainingCount++;
 }
 
@@ -59,16 +65,18 @@ function startServer(){
       ">>>>\n" +
       JSON.stringify(this.req.body) +
       "\n");
-    this.res.json('{"result":"ok"}');
+    bayes.train(this.req.body.fingerprint.data, this.req.body.classification);
+    //console.log("replying with:" + JSON.stringify(bayes.toJSON()) + "\n");
+    this.res.json(bayes.toJSON());
   });
 
   app.router.get('/training', function(){
-        console.log('received training data request');
-        fs.appendFile('app.log',
-          ">>>>\ntraining:" +
-          JSON.stringify(this.req.body) + 
-          "\n");
-        this.res.json(JSON.stringify(bayes.toJSON()));
+    console.log('received training data request');
+    fs.appendFile('app.log',
+      ">>>>\ntraining:" +
+      JSON.stringify(this.req.body) + 
+      "\n");
+    this.res.json(bayes.toJSON());
   });
 
   console.log("starting server on port " + port.toString() + "\n");
