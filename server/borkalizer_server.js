cc923022b0b96ebfa48cc1bfcc108afd9bbb596e
Drew Shafer <drew@bluegrass.drewshafer.com>
2012-08-30 00:26:51 -0500

    server support for user feedback using full dictionaries

--- server/borkalizer_server.js
+++ server/borkalizer_server.js
@@ -7,28 +7,33 @@ var app = flatiron.app;
 app.use(flatiron.plugins.http);
 
 var classified_log = 'classified.log';
-var app_log = 'app.log';
+var training_log = 'training.log';
 var port = 8080;
 
 var bayes = new classifier.Bayesian();
-if(!fs.existsSync(classified_log)){
-  console.log("no initial classification data available at classified.log\n");
-  startServer();
-} else {
-  console.log("reading initial classification data from classified.log\n");
+onEachLineOf(classified_log, '>>>>', trainIndividual, function(){
+  onEachLineOf(training_log, '>>>>', trainDictionary, startServer);
+});
 
-  var input = fs.createReadStream(classified_log);
+function onEachLineOf(fileName, lineSeparator, lineCallback, onDone){
+  if(!fs.existsSync(fileName)){
+    if(typeof(onDone)=='function'){
+      onDone();
+    }
+    return;
+  }
+  var input = fs.createReadStream(fileName);
   var remaining = '';
   var trainingCount = 0;
   input.on('data', function(data){
     remaining += data;
-    var index = remaining.indexOf('>>>>');
+    var index = remaining.indexOf(lineSeparator);
     var last = 0;
     while(index>-1){
       var line = remaining.substring(last,index);
       last = index+4;
       if(line.length>0){
-        trainLine(line);
+        lineCallback(line);
       }
       index = remaining.indexOf('>>>>', last);
     }
@@ -37,45 +42,54 @@ if(!fs.existsSync(classified_log)){
   
   input.on('end', function() {
     if(remaining.length>0){
-      trainLine(remaining);
+      lineCallback(remaining);
     }
-    console.log('finished initial training; count is ' + trainingCount);
-    startServer();
+    if(typeof(onDone)=='function'){
+      onDone();
+    }
   });
 }
 
-function trainLine(line){
+function trainDictionary(line){
+  var trainingData = JSON.parse(line);
+  bayes.backend.incCounts(trainingData.cats, trainingData.words);
+}
+function trainIndividual(line){
   var trainingData = JSON.parse(line);
   bayes.train(trainingData.fingerprint.data, trainingData.classification);
-  trainingCount++;
 }
 
 function startServer(){
   console.log("setting up HTTP routes");
-  app.router.get('/', function () {
-    console.log('received request on root');
-    this.res.writeHead(200, { 'Content-Type': 'text/plain' });
-    this.res.end('borkalizer!\n');
-  });
 
+  // clients send individual classification data to /classified
   app.router.post('/classified', function(){
     console.log('received classified request:');
     console.log(this.req.body);
-    fs.appendFile('classified.log', 
+    fs.appendFile(classified_log, 
       ">>>>\n" +
       JSON.stringify(this.req.body) +
       "\n");
     bayes.train(this.req.body.fingerprint.data, this.req.body.classification);
-    //console.log("replying with:" + JSON.stringify(bayes.toJSON()) + "\n");
     this.res.json(bayes.toJSON());
   });
 
+  // clients send completed dictionaries to /training
+  app.router.post('/training', function(){
+    console.log('received training post:');
+    console.log(this.req.body);
+    fs.appendFile(training_log, 
+      ">>>>\n" +
+      JSON.stringify(this.req.body) +
+      "\n");
+    bayes.backend.incCounts(this.req.body.cats, this.req.body.words);
+    this.res.json(bayes.toJSON());
+  });
+
+  // clients request most recent dictionary from /training
   app.router.get('/training', function(){
     console.log('received training data request');
-    fs.appendFile('app.log',
-      ">>>>\ntraining:" +
-      JSON.stringify(this.req.body) + 
-      "\n");
+    this.res.writeHead(200, { 'Content-Type': 'application/json' });
     this.res.json(bayes.toJSON());
   });
 
